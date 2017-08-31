b986d211d69380f9b98089004d52d14cb676f70c
rthomaz <rodrigothomaz@msn.com>
2017-08-31 16:29:12 -0300

    ok

--- scripts/memberserver-install.sh
+++ scripts/memberserver-install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-. ./source/common.sh
-
 echo ""
 echo "Iniciando execução do script $0"
 
@@ -35,41 +33,42 @@ if [ ! -d "source" ]; then
         mkdir source
 fi
 
+. ./source/common.sh
+
 echo "Fazendo download dos pacotes base ..."
 
-wget -O source/common.sh https://raw.githubusercontent.com/rthomaz/Infraestrutura/master/scripts/common.sh
+wget --no-cache -O source/common.sh https://raw.githubusercontent.com/rthomaz/Infraestrutura/master/scripts/common.sh
 chmod 700 source/common.sh
 
-wget -O source/package-install.sh https://raw.githubusercontent.com/rthomaz/Infraestrutura/master/scripts/package-install.sh
+wget --no-cache -O source/package-install.sh https://raw.githubusercontent.com/rthomaz/Infraestrutura/master/scripts/package-install.sh
 chmod 700 source/package-install.sh
 
 
 
-
 # clamav
-wget -O source/clamav-install.sh https://raw.githubusercontent.com/rthomaz/Infraestrutura/master/scripts/clamav-install.sh
+wget --no-cache -O source/clamav-install.sh https://raw.githubusercontent.com/rthomaz/Infraestrutura/master/scripts/clamav-install.sh
 executescript "clamav-install.sh"
 chmod 700 source/clamav-install.sh
 
-wget -O source/clamav-config.sh https://raw.githubusercontent.com/rthomaz/Infraestrutura/master/scripts/clamav-config.sh
+wget --no-cache -O source/clamav-config.sh https://raw.githubusercontent.com/rthomaz/Infraestrutura/master/scripts/clamav-config.sh
 executescript "clamav-config.sh"
 chmod 700 source/clamav-config.sh
 
 # ntpdate
-wget -O source/ntpdate-install.sh https://raw.githubusercontent.com/rthomaz/Infraestrutura/master/scripts/ntpdate-install.sh
+wget --no-cache -O source/ntpdate-install.sh https://raw.githubusercontent.com/rthomaz/Infraestrutura/master/scripts/ntpdate-install.sh
 executescript "ntpdate-install.sh"
 chmod 700 source/ntpdate-install.sh
 
-wget -O source/ntpdate-config.sh https://raw.githubusercontent.com/rthomaz/Infraestrutura/master/scripts/ntpdate-config.sh
+wget --no-cache -O source/ntpdate-config.sh https://raw.githubusercontent.com/rthomaz/Infraestrutura/master/scripts/ntpdate-config.sh
 executescript "ntpdate-config.sh -n $_pdc_server_fqdn"
 chmod 700 source/ntpdate-config.sh
 
 # ntp
-wget -O source/ntp-install.sh https://raw.githubusercontent.com/rthomaz/Infraestrutura/master/scripts/ntp-install.sh
+wget --no-cache -O source/ntp-install.sh https://raw.githubusercontent.com/rthomaz/Infraestrutura/master/scripts/ntp-install.sh
 executescript "ntp-install.sh"
 chmod 700 source/ntp-install.sh
 
-wget -O source/ntpdate-config.sh https://raw.githubusercontent.com/rthomaz/Infraestrutura/master/scripts/ntpdate-config.sh
+wget --no-cache -O source/ntpdate-config.sh https://raw.githubusercontent.com/rthomaz/Infraestrutura/master/scripts/ntpdate-config.sh
 executescript "ntp-config.sh -n $_pdc_server_fqdn"
 chmod 700 source/ntpdate-config.sh
 
